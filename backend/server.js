d4ad23eae89295d00518997e439b1da826c3f24c
Votha-Chheng <votha_chheng@gmx.fr>
2023-10-07 18:09:39 +0200

    Préparation déploiement sur Vercel

--- backend/server.js
+++ backend/server.js
@@ -42,17 +42,17 @@ app.use('/api/secretCode', secretCodeRoutes)
 
 app.get(`/api/config/paypal`, (req, res)=> res.send(process.env.PAYPAL_CLIENT_ID))
 
-const __dirname = path.resolve()
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+// const __dirname = path.resolve()
+// if(process.env.NODE_ENV === "production"){
+//   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get("*", (req, res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+//   app.get("*", (req, res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 
-} else {
-  app.get('/', (req, res)=>{
-    res.send('API is running...')
-  })
-}
+// } else {
+//   app.get('/', (req, res)=>{
+//     res.send('API is running...')
+//   })
+// }
 
 app.use(notFound)
 app.use(errorHandler)
